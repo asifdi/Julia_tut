d0bf9bf6ac7d5c7033b4ec2c9971a9afa1c36192
asifdi <diasif90@gmail.com>
2019-03-20 19:36:31 +0530

    inverse matrix

---
+++ Tutorials/Try 3.jl
@@ -0,0 +1,8 @@
+println("Hello")
+
+
+if true
+    println("true")
+else
+    println("false")
+end
---
+++ Tutorials/Try 4.jl
@@ -0,0 +1,8 @@
+using Plots
+plot(rand(5,5))
+
+if true
+    Println("true")
+else
+    Println("false")
+end
