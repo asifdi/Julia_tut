26e4abcc3dfa2f3d79a0ba2526ac872ca4977246
asifdi <diasif90@gmail.com>
2019-03-30 00:30:31 +0530

    try 9

---
+++ try 9.jl
@@ -0,0 +1,18 @@
+using Plots; gr()
+alphas = [0.0, 0.5, 0.98]
+T = 200
+
+series = []
+labels = []
+
+for alpha in alphas
+    x = zeros(T+1)
+    x[1] = 0.0
+    for t in 1:T
+        x[t+1] = alpha * x[t] + randn()
+    end
+    push!(series, x)
+    push!(labels, "alpha = $alpha")
+end
+
+plot(series, labels=reshape(labels,1,length(labels)), w = 2)
