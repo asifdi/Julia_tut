b22d9a3e2d5b0e5a96f95b47cf4f087f728147e6
asifdi <diasif90@gmail.com>
2019-03-29 15:52:50 +0530

    updated input commands, , partly successful

--- Tutorials/try 5.jl
+++ Tutorials/try 5.jl
@@ -1,1 +1,9 @@
-input(number="")
+using Dates
+println()
+print("enter year   "); y = (readline(stdin));
+print("enter month  "); m = (readline(stdin));
+print("enter day    "); d = (readline(stdin));
+
+date = Date('y', 'm', 'd')
+
+println(date::stdin)
---
+++ Tutorials/try 7.jl
@@ -0,0 +1,6 @@
+print("enter a number: "); user_input = readline();
+for user_input % 2 == 0
+    print("even")
+else
+    print("Odd")
+End
