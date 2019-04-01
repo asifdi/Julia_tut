d2984b265ee2638609cb9a417506b98ec9e5231b
asifdi <diasif90@gmail.com>
2019-04-01 17:59:51 +0530

    general forward kinematic solution for 2 DOF

--- Tutorials/input.jl
+++ Tutorials/input.jl
@@ -1,8 +1,45 @@
-function input(prompt::Float64="")
-    print(prompt)
-    return chomp(readline())
+# Forward kinematics for 2 DOF
+global l1, l2
+print("Enter first link length in mm = "); l1 = parse(Float64,readline())
+while l1 <= 0
+    global l1
+    if l1 < 0
+        println("Length cannot be negative")
+        print("Enter first link length in mm = "); l1 = parse(Float64,readline())
+    else l1 == 0
+        println("Length cannot be zero")
+        print("Enter first link length in mm = "); l1 = parse(Float64,readline())
+    end
 end
 
-Px= input("Enter x-coordinate of target: ")::Int64;
-Py= input("Enter y-coordinate of target: ")::Int64;
-println("$Px + $Py = $(Px+Py)")
+print("Enter second link length in mm = "); l2 = parse(Float64, readline())
+while l2 <= 0
+global l2
+    if l2 < 0
+        println("Length cannot be negative")
+        print("Enter second link length in mm = "); l2 = parse(Float64,readline())
+    else l2 == 0
+        println("Length cannot be zero")
+        print("Enter second link length in mm = "); l2 = parse(Float64,readline())
+    end
+end
+
+global t1, t2
+print("Enter Joint 1 angle in degrees = "); t1 = parse(Float64, readline())
+while !(-90.00 < t1 < 90.00)
+    global t1
+    println("first angle out of range [-90 , 90]")
+    print("Enter Joint 1 angle in degrees = "); t1 = parse(Float64, readline())
+end
+
+print("Enter Joint 2 angle in degrees = "); t2 = parse(Float64, readline())
+while !(-90.00 < t2 < 90.00)
+    global t1
+    println("second angle out of range [-90 , 90]")
+    print("Enter Joint 2 angle in degrees = "); t1 = parse(Float64, readline())
+end
+
+# Forward kinematic calculations
+x = ((l1 * sind(t1)) + (l2 * sind(t1 + t2)));
+y = ((l1 * cosd(t1)) + (l2 * cosd(t1 + t2)));
+println("Goal position =  $x, $y")
