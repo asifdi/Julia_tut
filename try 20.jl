9ce4f75f9f743c2b0eac16862f27e99881e1c90d
asifdi <diasif90@gmail.com>
2019-06-24 22:35:16 +0530

    first commit

--- try 20.jl
+++ try 20.jl
@@ -1,7 +1,8 @@
-using Plots
-x = collect(0:1:1000);
-y = zeros(length(x));
-for i in range(1,length=length(x))
-    y[i] = ((-11*x[i]^3) + (1.2*x[i]^2) + (0.9*x[i]) + 0.3);
-end 
-plot(x,y)
+using VegaDatasets, VegaLite
+dataset("cars") |> @vlplot(:point, x=:Accelaration, y=:Miles_per_Gallon)
+
+if true
+    println("TRUE")
+else
+    println("False")
+end
