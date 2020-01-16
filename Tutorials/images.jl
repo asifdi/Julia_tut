007f297a88b91cfaaf16501920579a3a5e59d606
asifdi <diasif90@gmail.com>
2020-01-16 21:46:58 +0530

    trying image functions

---
+++ Tutorials/images.jl
@@ -0,0 +1,28 @@
+using Images, TestImages, Colors
+methods(testimage)
+img = testimage("lighthouse")
+summary(img)
+img[1]
+img[0,0]
+@which []
+
+c = img[10];
+println(summary(img));
+println(c.r, " ", c.g, " ", c.b);
+println(red(c), " ", green(c), " ", blue(c))
+
+img_bgr = BGR.(img);
+
+println(summary(img_bgr));
+c = img_bgr[10];
+println(c.r, " ", c.g, " ", c.b)
+println(red(c), " ", green(c), " ", blue(c))
+
+#img = load("skyline.jpg")
+
+println(size(img))
+summary(img)
+
+#tiled_img = Array{RGB{N0f8}, 2}(size(512, 768))
+#linspace(1, size(img, 1), 8)
+''' codes are obselte, exploring new codes'''
